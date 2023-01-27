48fd0402b081e58140b3883f30b281082a306708
harm <el.harmo@gmail.com>
2023-01-27 11:04:48 +0100

    Using iterators but still run out of call stack hmmmmmmmmmm

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use std::collections::HashMap;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::ops::Add;
+use std::rc::Rc;
+use std::vec::IntoIter;
 
 #[derive(PartialEq, Eq, Debug, Hash, Clone)]
 enum Term<T>
@@ -43,26 +45,27 @@ where
     panic!("Illegal");
 }
 
-type Stream<T> = Vec<(Substitution<T>, T)>;
+type Stream<T> = dyn Iterator<Item=(Substitution<T>, T)>;
 
-fn mzero<T>() -> Stream<T>
+fn mzero<T>() -> Box<Stream<T>>
 where
-    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,
+    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>+ 'static,
 {
-    Vec::new()
+    let vec: Vec<(Substitution<T>, T)> = Vec::new();
+    Box::new(vec.into_iter())
 }
 
 type Goal<T: Debug + Hash + Eq + Clone + Add<i32, Output = T>> =
-    dyn Fn((Substitution<T>, T)) -> Stream<T>;
+    dyn Fn((Substitution<T>, T)) -> Box<Stream<T>>;
 
 // Returns a goal
-fn eq<T>(u: Term<T>, v: Term<T>) -> impl Fn((Substitution<T>, T)) -> Stream<T>
+fn eq<T>(u: Term<T>, v: Term<T>) -> impl Fn((Substitution<T>, T)) -> Box<Stream<T>>
 where
-    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,
+    T: Debug + Hash + Eq + Clone + Add<i32, Output = T> + 'static,
 {
     move |(s, c): (Substitution<T>, T)| {
         if let Some(s2) = unify(&u, &v, s) {
-            vec![(s2, c)]
+            Box::new(vec![(s2, c)].into_iter())
         } else {
             mzero()
         }
@@ -97,7 +100,7 @@ where
 }
 
 // returns a goal
-fn call_fresh<T>(f: impl Fn(Term<T>) -> Box<Goal<T>>) -> impl Fn((Substitution<T>, T)) -> Stream<T>
+fn call_fresh<T>(f: impl Fn(Term<T>) -> Box<Goal<T>>) -> impl Fn((Substitution<T>, T)) -> Box<Stream<T>>
 where
     T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,
 {
@@ -105,49 +108,81 @@ where
 }
 
 //Returns a Goal
-fn disj<T>(g1: Box<Goal<T>>, g2: Box<Goal<T>>) -> impl Fn((Substitution<T>, T)) -> Stream<T>
+fn disj<T>(g1: Box<Goal<T>>, g2: Box<Goal<T>>) -> impl Fn((Substitution<T>, T)) -> Box<Stream<T>>
 where
-    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,
+    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>+ 'static,
 {
     move |(s, c): (Substitution<T>, T)| mplus(g1((s.clone(), c.clone())), g2((s, c)))
 }
 
-fn conj<T>(g1: Box<Goal<T>>, g2: Box<Goal<T>>) -> impl Fn((Substitution<T>, T)) -> Stream<T>
+fn conj<T>(g1: Box<Goal<T>>, g2: Rc<Box<Goal<T>>>) -> impl Fn((Substitution<T>, T)) -> Box<Stream<T>>
 where
+    T: Debug + Hash + Eq + Clone + Add<i32, Output = T> + 'static,
+{
+    move |(s, c): (Substitution<T>, T)| bind(g1((s, c)), g2.clone()) // TODO was this but won't work with using g2.... Can't move..
+
+    // move |(s, c): (Substitution<T>, T)| {
+    //     let mut ret = Vec::new();
+    //     // TODO not infinite
+    //     for st in g1((s, c)) {
+    //         let mut x = g2(st.clone());
+    //         if !x.is_empty() {
+    //             ret.append(&mut x);
+    //         }
+    //     }
+    //     return ret;
+    // }
+}
+
+struct DoubleIt<T> where
+    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,{
+    c: u32,
+    s1: Box<Stream<T>>,
+    s2: Box<Stream<T>>
+}
+
+impl<T> Iterator for DoubleIt<T>  where
     T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,
 {
-    // move |(s, c): (Substitution<T>, T)| bind(g1((s, c)), g2) TODO was this but won't work with using g2.... Can't move..
-    move |(s, c): (Substitution<T>, T)| {
-        let mut ret = Vec::new();
-        // TODO not infinite
-        for st in g1((s, c)) {
-            let mut x = g2(st.clone());
-            if !x.is_empty() {
-                ret.append(&mut x);
+    type Item = (Substitution<T>,T);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        println!("next from mplus");
+        if self.c % 2 == 0 {
+            if let Some(n) = self.s1.next() {
+                return Some(n);
             }
         }
-        return ret;
+            self.s2.next()
     }
 }
 
-fn mplus<T>(mut st1: Stream<T>, mut st2: Stream<T>) -> Stream<T>
+fn mplus<T>(mut st1: Box<Stream<T>>, mut st2: Box<Stream<T>>) -> Box<Stream<T>>
 where
-    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,
+    T: Debug + Hash + Eq + Clone + Add<i32, Output = T> + 'static,
 {
-    // TODO not infinite
-    st1.append(&mut st2);
-    st1
+
+    //infinite fine but I guess, problemsssssss.... With the eval model. COuld chain but doesn't matter, all the boxes....
+    Box::new(DoubleIt {
+        c: 0,
+        s1: st1,
+        s2: st2,
+    })
+
+    // // TODO not infinite
+    // st1.append(&mut st2);
+    // st1
 }
 
-fn bind<T>(mut st: Stream<T>, g: Box<Goal<T>>) -> Stream<T>
+fn bind<T>(mut st: Box<Stream<T>>, g: Rc<Box<Goal<T>>>) -> Box<Stream<T>>
 where
-    T: Debug + Hash + Eq + Clone + Add<i32, Output = T>,
+    T: Debug + Hash + Eq + Clone + Add<i32, Output = T> + 'static,
 {
     // TODO not infinite
-    if st.is_empty() {
-        mzero()
+    if let Some(x) = st.next() {
+        mplus(g(x), bind(st, g))
     } else {
-        mplus(g(st.pop().unwrap()), bind(st, g))
+        Box::new(mzero())
     }
 }
 
@@ -155,29 +190,39 @@ fn empty_state() -> (Substitution<i32>, i32) {
     (HashMap::new(), 0)
 }
 
-fn a_and_b() -> impl Fn((Substitution<i32>, i32)) -> Stream<i32> {
+fn a_and_b() -> impl Fn((Substitution<i32>, i32)) -> Box<Stream<i32>> {
     conj(
         Box::new(call_fresh(|a| Box::new(eq(a, Object(7))))),
-        Box::new(call_fresh(|b| {
+        Rc::new(Box::new(call_fresh(|b| {
             Box::new(disj(
                 Box::new(eq(b.clone(), Object(5))),
                 Box::new(eq(b.clone(), Object(6))),
             ))
-        })),
+        }))),
     )
 }
 
-fn fives(x: Term<i32>) -> impl Fn((Substitution<i32>, i32)) -> Stream<i32> {
+fn fives(x: Term<i32>) -> impl Fn((Substitution<i32>, i32)) -> Box<Stream<i32>> {
+    // println!("fives");
+    //Ah crap execution, ..... Hmmmmmmmmm
     Box::new(disj(Box::new(eq(x.clone(), Object(5))), Box::new(fives(x))))
 }
 
 fn main() {
     let trying = call_fresh(|q| Box::new(eq(q, Object(5))))(empty_state());
-    println!("Hello, world! {trying:?}");
-    let pffff2 = a_and_b()(empty_state());
-    println!("Whee {pffff2:?}");
+    for t in trying {
+        println!("Hello, world! {t:?}");
+    }
 
-    // Stack overfllllllllllow!
+    let pffff2 = a_and_b()(empty_state());
+    for t in pffff2 {
+        println!("Whee {t:?}");
+    }
+
+
+    // // Stack overfllllllllllow!
     let breaking = call_fresh(|x| Box::new(fives(x)))(empty_state());
-    println!("Break {breaking:?}")
+    for t in breaking {
+        println!("Break {t:?}")
+    }
 }
